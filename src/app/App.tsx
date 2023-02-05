fc1ab9ea6afe79b0bb932cd864099cefdb64e2ad
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2023-02-05 12:53:12 +0300

    подготовили структуру

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
 import { Suspense } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import './styles/index.scss'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { useTheme } from './theme/useTheme'
-import { classNames } from './helpers/classNames/classNames'
+
+import { useTheme } from 'app/providers/ThemeProvider'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
+    /* итоговый класс будет class="app dark || light", 
+      если app-show будет = true, класс станет class="app dark || light app-show"
+    */
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Main</Link>
@@ -17,8 +21,8 @@ const App = () => {
       {/* позволяет асинхронно подгружать страницы */}
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,11 +1,9 @@
 import { FC, useMemo, useState } from 'react'
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext'
 
 // если в локалстораге есть значение беру от туда, если нет то беру светлую тему
 // так как локалстораге всегда возвращает строку, мне нужно ее преобразовать в тип Themeс помощью оператора as
 const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
-
-console.log(defaultTheme, defaultTheme)
 
 const ThemeProvider: FC = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme)
