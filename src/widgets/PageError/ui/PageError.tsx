7fb060fc35132ef3806b7bf9e457a6080db1c09f
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2023-02-13 16:01:17 +0300

    add errorboundary, add budnle analyzer

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+    className?: string;
+}
+
+const resetPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+};
+
+export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.PageError, {}, [])}>
+            <p>{t('Произошла ошибка')}</p>
+            <Button onClick={resetPage}>{t('обновить страницу')}</Button>
+        </div>
+    );
+};
