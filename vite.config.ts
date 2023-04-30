465a0f3d4d7f66f827c844b522da43941a85181d
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2023-04-30 11:31:50 +0300

    add vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [svgr({ exportAsDefault: true }), react()],
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+    css: {
+        modules: {
+            generateScopedName: '[path][name]__[local]--[hash:base64:5]',
+        },
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:8000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
