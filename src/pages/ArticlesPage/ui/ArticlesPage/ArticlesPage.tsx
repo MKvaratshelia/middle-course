41b504613371e0d5a5ac70fa941912b1b29e4fae
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2023-05-16 14:09:53 +0300

    add e2e tests

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -45,6 +45,7 @@ const ArticlesPage: FC<ArticlesPageProps> = (props) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
             <Page
+                data-testid='ArticlesPage'
                 onScrollEnd={onLoadNextPart}
                 className={classNames(cls.ArticlesPage, {}, [className])}
             >
